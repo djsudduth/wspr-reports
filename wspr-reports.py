5b5f3c64b071457a1e732f45c3baa48c9fe48663
Don Sudduth <don.sudduth@gmail.com>
2023-05-28 13:46:00 -0500

    fixing timestamp

--- wspr-reports.py
+++ wspr-reports.py
@@ -5,18 +5,23 @@ import numpy as np
 import pandas as pd
 from bisect import bisect
 
-
+WSPR_HEADER_ERR = "Your wspr file must add a tab between the words Date and Time in the DateTime column that defaults from wsprnet.org"
 
 def open_wspr_file():
     #open the wspr data into a dataframe and add a clean timestamp column
-    df = pd.read_csv("wspr.txt", delimiter=r"\s+")
-    df['TimeStamp'] = pd.to_datetime(df['Date'] + "T" + df['Time'] + ":00Z")
-    df = df.sort_values('TimeStamp')
-    print (df.head())
+    df = pd.read_csv("wspr.txt", sep='\t')
+ 
+    if 'Timestamp' in df.columns:
+        df['Timestamp'] = df['Timestamp'].str.strip()
+        df['Timestamp'] = df['Timestamp'].str.replace(" ", "T")
+        df['DateTime'] = pd.to_datetime(df['Timestamp'] + ":00Z")
+        df = df.sort_values('DateTime')
+        print (df.to_string())
+    else:
+        print ("\n" + WSPR_HEADER_ERR)
 
 
 def main():
-
     try:
         #Read wspr data from wsprnet.org
         open_wspr_file()
@@ -27,7 +32,5 @@ def main():
 
 
 #Version 0.0.1
-
 if __name__ == '__main__':
-
     main()  # pylint: disable=no-value-for-parameter
