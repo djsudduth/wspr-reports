8f753cc40b7ef0fc4e408d31b1de77adf0527290
Don Sudduth <don.sudduth@gmail.com>
2023-05-29 12:14:31 -0500

    trending snr slopes

--- wspr-reports.py
+++ wspr-reports.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from bisect import bisect
 
-WSPR_HEADER_ERR = "Your wspr file cannot have spaces in the data headers that defaults from wsprnet.org"
+WSPR_HEADER_ERR = "Your wspr file cannot be missing or have spaces in the data headers that defaults from wsprnet.org"
 
 def open_wspr_file():
     #open the wspr data into a dataframe and add a clean timestamp column
@@ -17,10 +17,12 @@ def add_wspr_dimensions(df):
         df['Timestamp'] = df['Timestamp'].str.strip()
         df['Timestamp'] = df['Timestamp'].str.replace(" ", "T")
         df['DateTime'] = pd.to_datetime(df['Timestamp'] + ":00Z")
+        df['Reporter'] = df['Reporter'].str.strip()
         df['map'] = pd.cut(df['az'], [0, 23, 68, 113, 158, 203, 248, 293, 337, 359], labels= ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'], ordered=False)
         df['drange'] = pd.cut(df['km'], [0, 800, 4000, 8000, 13000], labels=['NEAR', 'MID', 'LONG', 'VLONG'])
         df = df.sort_values('DateTime')
         print (df.to_string() + "\n\n")
+        #print (df.loc[(df['map'] == 'NW') & (df['drange'] == 'MID')])
     else:
         print ("\n" + WSPR_HEADER_ERR)
 
@@ -28,26 +30,49 @@ def get_wspr_snr_trends(df):
     slopes = []
     stdvs = []
     variances = []
+    maps = []
+    kms = []
 
-    df = df.groupby('Reporter').agg({'SNR':list}).reset_index()
+    df2 = df.groupby(['map', 'km', 'Reporter'])['SNR'].describe()
+    print (df2.to_string() + "\n\n")
 
-    for index, row in df.iterrows():
-        if len(row['SNR']) > 2: #only calculate for 3 or more reports from the same reporter
+    df2 = df2.reset_index()
+    #print(df2.groupby('map')['std'].mean() + "\n\n")
+  
+
+    df2 = df.groupby(['km', 'Reporter']).agg({'SNR':list}).reset_index()
+
+    for index, row in df2.iterrows():
+        if len(row['SNR']) > 1: #only calculate for 3 or more reports from the same reporter
             s = list(range(len(row['SNR'])))
             slope, intercept = statistics.linear_regression(s, row['SNR'])
-            stdv = statistics.stdev(row['SNR'])
+            stdv =  statistics.stdev(row['SNR'])
             variance = statistics.variance(row['SNR'])
         else:
             slope = 0
             stdv = 0
             variance = 0
-        slopes.append(slope)
-        stdvs.append(stdv)
-        variances.append(variance)
-    df['slopes'] = slopes
-    df['stdvs'] = stdvs
-    df['variances'] = variances
-    print (df.to_string())
+        #slopes.append("{:.2f}".format(slope))
+        slopes.append(float(slope))
+        stdvs.append("{:.2f}".format(stdv))
+        variances.append("{:.2f}".format(variance))
+        maps.append(df.loc[(df['Reporter'] == row['Reporter'])].iloc[0]['map'])
+    df2['slopes'] = slopes
+    df2['stdvs'] = stdvs
+    df2['variances'] = variances
+    df2['map'] = maps
+    print (df2.to_string() + "\n\n")
+    df2 = df2.reset_index()
+    #print (df.loc[(df['Reporter'] == row['Reporter'])].iloc[0]['map'])
+    
+    df2 = df2.groupby('map').agg({'slopes':list}).reset_index()
+    df2['Mean'] = [np.array(x).mean() for x in df2.slopes.values]
+    print (df2.to_string())
+
+
+ 
+    
+
 
 
 
@@ -69,3 +94,7 @@ def main():
 #Version 0.0.1
 if __name__ == '__main__':
     main()  # pylint: disable=no-value-for-parameter
+
+
+#Saved queries
+#print (df2.loc[(df2['map'] == 'SE') & (df2['km'] < 600)])
